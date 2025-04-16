b16336da1da4882bbe6a60c0888b63512dc3c9ca
Arian Rodriguez <arianmrv12@gmail.com>
2025-04-15 23:04:17 -0500

    add(logout): removing data from localstorage

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 import logoutIcon from "../assets/icons/logout.svg";
 import logoutFilledIcon from "../assets/icons/logout-filled.svg";
+import { useNavigate } from "react-router-dom";
 
-const LogOut = ({ onClick }) => {
+const LogOut = () => {
+    const navigate = useNavigate();
     const [isHovered, setIsHovered] = useState(false);
+
+    const logOut = () => {
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+
+        // removing from localstorage the mapbox key with regex: mapbox.eventData.*
+        const mapboxKey = Object.keys(localStorage).find((key) => key.startsWith("mapbox.eventData."));
+        if (mapboxKey) {
+            console.log(`Removing Mapbox key: ${mapboxKey}`);
+            localStorage.removeItem(mapboxKey);
+        }
+
+        navigate("/sign-in", { replace: true });
+    }
 
     return (
         <div className="absolute bottom-0 left-0 w-full" style={{ padding: ".8rem" }}>
             <div className="w-full h-[1px] bg-[#C4C4C4] my-3"></div>
             <div
                 className="flex items-center gap-3 cursor-pointer rounded-md"
-                onClick={onClick}
+                onClick={logOut}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
                 style={{
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = ({ children }) => (
 
         {children}
 
-        <LogOut onClick={() => {}} />
+        <LogOut />
     </div>
 )
 
