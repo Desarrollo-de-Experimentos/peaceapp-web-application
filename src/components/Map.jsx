dbfdbc01e03bdd50a4c68efaec446a0399650a93
Arian Rodriguez <arianmrv12@gmail.com>
2025-04-15 23:34:36 -0500

    add(map): user's marker location and event listening

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,10 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { obtainCurrentLocation } from '../utils/currentLocation.js';
 import IconOption from './IconOption.jsx';
 import plusIcon from '../assets/icons/plus.svg';
 import IconOptionList from './IconOptionList.jsx';
-
 
 const Map = ({ onNewReportClick }) => {
     /**
@@ -16,61 +15,84 @@ const Map = ({ onNewReportClick }) => {
     const mapboxToken = import.meta.env.VITE_MAPBOX_TOKEN;
     if (!mapboxToken) throw new Error("Mapbox token is not defined");
 
-    const mapRef = useRef();
-    const mapContainerRef = useRef();
+    const mapRef = useRef(null);
+    const mapContainerRef = useRef(null);
+    const markerRef = useRef(null);
 
     const [location, setLocation] = useState(null);
 
     useEffect(() => {
         const fetchLocation = async () => {
             const { latitude, longitude } = await obtainCurrentLocation();
-            console.log("Latitude: ", latitude, "Longitude: ", longitude);
             setLocation({ latitude, longitude });
         };
         fetchLocation();
     }, []); // getting the current location only once
 
     useEffect(() => {
-        if (location && !mapRef.current) {
-            // only initializing the map if it hasn't been initialized yet
-            mapboxgl.accessToken = mapboxToken;
-            mapRef.current = new mapboxgl.Map({
-                container: mapContainerRef.current,
-                center: [location.longitude, location.latitude],
-                zoom: 16,
-                attributionControl: false,
-            });
+        if (!location || !mapContainerRef.current || mapRef.current) return;
 
-            // listen to map load event, this ensures the map loads only once, verify the console for the message
-            mapRef.current.on('load', () => {
-                console.log("Map loaded");
-            });
-        } else if (location && mapRef.current) {
-            // updating the map's center and zoom if location changes
-            mapRef.current.setCenter([location.longitude, location.latitude]);
-            mapRef.current.setZoom(16);
-        }
+        mapboxgl.accessToken = mapboxToken;
 
+        mapRef.current = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            center: [location.longitude, location.latitude],
+            zoom: 16,
+            attributionControl: false,
+        });
+
+        const userMarkerEl = document.createElement('div');
+        userMarkerEl.className = 'custom-user-marker';
+        userMarkerEl.style.backgroundImage = "url('src/assets/icons/current-location.svg')";
+        userMarkerEl.style.width = '60px';
+        userMarkerEl.style.height = '60px';
+        userMarkerEl.style.backgroundSize = 'contain';
+        userMarkerEl.style.backgroundRepeat = 'no-repeat';
+        userMarkerEl.style.zIndex = 1;
+
+        // listen to map load event, this ensures the map loads only once, verify the console for the message
+        mapRef.current.on("load", () => {
+            console.log("Map loaded");
+
+            // marking the user's location
+            markerRef.current = new mapboxgl.Marker(userMarkerEl)
+                .setLngLat([location.longitude, location.latitude])
+                .addTo(mapRef.current);
+        });
+
+        // listen to map move event, this ensures the marker moves with the map
+        mapRef.current.on("move", () => {
+            const center = mapRef.current.getCenter();
+            console.log("Map center:", center);
+            markerRef.current?.setLngLat([center.lng, center.lat]);
+        });
+
+        mapRef.current.on('movestart', () => {
+            userMarkerEl.classList.add('lifted');
+        });
+    
+        mapRef.current.on('moveend', () => {
+            userMarkerEl.classList.remove('lifted');
+        });
+        
         return () => {
-            if (mapRef.current) {
-                mapRef.current.remove();
-            }
+            mapRef.current?.remove();
         };
     }, [location]); // only running when the location changes
 
-    if (!location) return <div>Loading the PeaceApp's Map</div>;
+    if (!location) return <div>Loading PeaceApp's Map...</div>;
 
     return (
-        <div id="map-container" ref={mapContainerRef} style={{ 
-                width: '80%', 
+        <div
+            ref={mapContainerRef}
+            style={{
+                width: '80%',
                 height: '100vh',
                 position: 'absolute',
                 top: 0,
                 right: 0,
-            }} 
-                
-            >
-
+            }}
+        >
             <div className="absolute top-0 left-0 z-10 flex items-center justify-center gap-2" style={{ padding: "0.2rem 0.8rem", margin: "1rem" }}>
                 <IconOption icon={plusIcon} text={"New report"} onClick={onNewReportClick} itemList={true} />
                 <IconOptionList />
